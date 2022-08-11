e3b86a2ebc053e1abbdffd83f0cc4bdd169fe4de
DiegoAlmeida <diegof.almeidas@gmail.com>
2022-08-11 14:04:29 -0300

    adding boxdecoration Stack

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -1,8 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HelloListView extends StatelessWidget {
+class Dog {
+  String nome;
+  String foto;
 
+  Dog(this.nome, this.foto);
+}
+
+class HelloListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,28 +15,52 @@ class HelloListView extends StatelessWidget {
         title: Text("ListView"),
       ),
       body: _body(),
-
     );
   }
 
   _body() {
-    return ListView(
-      children: <Widget>[
-        _img("assets/images/dog1.png"),
-        _img("assets/images/dog2.png"),
-        _img("assets/images/dog3.png"),
-        _img("assets/images/dog4.png"),
-        _img("assets/images/dog5.png"),
-      ],
+    List<Dog> dogs = [
+      Dog("Jack Russel", "assets/images/dog1.png"),
+      Dog("Labrador", "assets/images/dog2.png"),
+      Dog("Pug", "assets/images/dog3.png"),
+      Dog("RottWeiler", "assets/images/dog4.png"),
+      Dog("Pastor", "assets/images/dog5.png"),
+    ];
+
+    return ListView.builder(
+      itemCount: dogs.length,
+      itemExtent: 300,
+      itemBuilder: (context, index) {
+        Dog dog = dogs[index];
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            _img(dog.foto),
+            Align(
+              alignment:  Alignment.topLeft,
+              child: Container(
+                margin: EdgeInsets.all(12),
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                child: Text(
+                  dog.nome,
+                  style: TextStyle(fontSize: 26, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
 
 _img(String img) {
-  return Container(
-      child: Image.asset(
-        img,
-        fit: BoxFit.cover,
-      )
+  return Image.asset(
+    img,
+    fit: BoxFit.cover,
   );
 }
